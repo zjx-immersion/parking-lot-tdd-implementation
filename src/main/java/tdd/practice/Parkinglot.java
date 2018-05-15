254a5b049697dd789cf16701c55b1f37823524df
zjx-immersion <jianxin_home@163.com>
2018-05-15 14:34:14 +0800

    [Jason]add the first test case and implementation for task one

---
+++ src/main/java/tdd/practice/Parkinglot.java
@@ -0,0 +1,16 @@
+package tdd.practice;
+
+/**
+ * Created by jxzhong on 2018/5/15.
+ */
+public class Parkinglot {
+    private final int totalPlaceNum;
+
+    public Parkinglot(int totalPlaceNum) {
+        this.totalPlaceNum = totalPlaceNum;
+    }
+
+    public int getAvailableNums() {
+        return 1;
+    }
+}
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,5 +1,23 @@
+import org.junit.Test;
+import tdd.practice.Parkinglot;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by jxzhong on 2018/5/15.
  */
 public class ParkingLotTest {
+
+    @Test
+    public void should_get_1_available_place_when_parking_lot_is_empty_and_1_total_place() throws Exception {
+        //given
+        Parkinglot parkinglot = new Parkinglot(1);
+        //when
+        int availableNum = parkinglot.getAvailableNums();
+
+        //then
+        assertEquals(availableNum, 1);
+    }
+
+
 }
