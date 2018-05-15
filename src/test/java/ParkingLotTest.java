116d8fd68561fcf6f96247585bc2e229963fb2d8
zjx-immersion <jianxin_home@163.com>
2018-05-15 15:18:44 +0800

    [Jason] refactor the ticker diff condition test case for second test

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -3,6 +3,7 @@ import tdd.practice.Parkinglot;
 import tdd.practice.Vehicle;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotSame;
 
 /**
  * Created by jxzhong on 2018/5/15.
@@ -23,27 +24,32 @@ public class ParkingLotTest {
     @Test
     public void should_parking_vehicle_success_when_call_packing() throws Exception {
         //given
-        Parkinglot parkinglot = new Parkinglot(1);
+        Parkinglot parkinglot = new Parkinglot(2);
         Vehicle vehicle = new Vehicle();
         //when
-        String ticket = parkinglot.parking(vehicle);
+        String ticket1 = parkinglot.parking(vehicle);
+        String ticket2 = parkinglot.parking(vehicle);
 
         //then
 
-        assertEquals(3, ticket.length());
+        assertEquals(3, ticket1.length());
+        assertEquals(3, ticket2.length());
+        assertNotSame(ticket2, ticket1);
     }
 
     @Test
-    public void should_get_indicated_vehicle_when_get_vehicle_via_park_ticket() throws Exception {
+    public void should_get_indicated_vehicle_when_get_vehicle_via_park_ticket_and_not_only_one_car_in_lot() throws Exception {
         //given
-        Parkinglot parkinglot = new Parkinglot(1);
-        Vehicle vehicle = new Vehicle();
-        String ticket = parkinglot.parking(vehicle);
+        Parkinglot parkinglot = new Parkinglot(2);
+        Vehicle vehicle1 = new Vehicle();
+        Vehicle vehicle2 = new Vehicle();
+        String ticket1 = parkinglot.parking(vehicle1);
+        String ticket2 = parkinglot.parking(vehicle2);
         //when
-        Vehicle pickedVehicle = parkinglot.pickUp(ticket);
+        Vehicle pickedVehicle = parkinglot.pickUp(ticket1);
 
         //then
-        assertEquals(vehicle, pickedVehicle);
+        assertEquals(vehicle1, pickedVehicle);
 
     }
 
